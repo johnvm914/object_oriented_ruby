225c297b0c86bf43d3583aa9c2dcfc5e8cdf830b
John Messina <johnvm914@gmail.com>
2017-01-09 19:59:02 -0500

    initial commit

--- store_items.rb
+++
@@ -1,3 +0,0 @@
-item1 = {:name => "tomato", :color => "red", :price => 0.50}
-item2 = {:name => "cucumber", :color => "green", :price => 0.75}
-item3 = {name: "banana", color: "yellow", price: 1.00}
