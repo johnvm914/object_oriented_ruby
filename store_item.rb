bf9a437b92adc8c06720822322359349dc73b86c
John Messina <johnvm914@gmail.com>
2017-01-09 21:22:21 -0500

    changed class input to hash

--- store_item.rb
+++ store_item.rb
@@ -3,15 +3,23 @@ class Item
   attr_reader :name, :color, :price
   attr_writer :price
 
-  def initialize(name, color, price)
-    @name = name
-    @color = color
-    @price = price
+  def initialize(items)
+    @name = items[:name]
+    @color = items[:color]
+    @price = items[:price]
+  end
+
+  def info
+    puts "A #{name} is the color #{color} and costs $#{price}"
   end
 
 end
 
-item1 = Item.new("tomato", "red", 0.50)
-item2 = Item.new("cucumber", "green", 0.75)
-item3 = Item.new("banana", "yellow", 1.00)
+item1 = Item.new(name: "tomato", color: "red", price: 0.50)
+item2 = Item.new(name: "cucumber", color: "green", price: 0.75)
+item3 = Item.new(name: "banana", color: "yellow", price: 1.00)
+
+item1.info
+
+
 
